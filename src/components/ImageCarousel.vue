795483314a052394cf44d69d6ae35906375bf810
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-04-18 03:08:54 +0400

    refactor ImageCarousel component for improved styling and autoplay functionality

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -5,37 +5,18 @@
     navigation
     padding
     infinite
-    autoplay
     arrows
     transition-prev="slide-right"
     transition-next="slide-left"
     @mouseenter="autoplay = false"
     @mouseleave="autoplay = true"
-    control-color="primary"
-    control-type="push"
-    class="rounded-borders"
-    style="height: 700px"
+    control-color="secondary"
+    class="carousel rounded-borders"
   >
-    <q-carousel-slide
-      :name="1"
-      style="height: fit-content"
-      img-src="../assets/slider_image_1.jpg"
-    />
-    <q-carousel-slide
-      :name="2"
-      style="height: fit-content"
-      img-src="../assets/slider_image_2.jpg"
-    />
-    <q-carousel-slide
-      :name="3"
-      style="height: fit-content"
-      img-src="../assets/slider_image_3.jpg"
-    />
-    <q-carousel-slide
-      :name="4"
-      style="height: fit-content"
-      img-src="../assets/slider_image_4.jpg"
-    />
+    <q-carousel-slide :name="1" img-src="../assets/slider_image_1.jpg" />
+    <q-carousel-slide :name="2" img-src="../assets/slider_image_2.jpg" />
+    <q-carousel-slide :name="3" img-src="../assets/slider_image_3.jpg" />
+    <q-carousel-slide :name="4" img-src="../assets/slider_image_4.jpg" />
   </q-carousel>
 </template>
 
@@ -44,3 +25,12 @@ import { ref } from 'vue'
 
 const slide = ref(1)
 </script>
+
+<style scoped>
+.carousel {
+  height: 100%;
+  max-height: 700px;
+  aspect-ratio: 16 / 7;
+  margin-inline: auto;
+}
+</style>
