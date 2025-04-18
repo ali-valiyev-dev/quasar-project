71c687bd4d7d73a8e1a12f3fe2d6706e27f7c463
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-04-18 04:33:03 +0400

    add StatisticsCard component

---
+++ src/components/StatisticsCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-card flat bordered class="q-pa-lg q-mx-auto stats-banner b">
+    <q-card-section class="column row-sm b" style="row-gap: 30px">
+      <q-item v-for="(stat, index) in stats" :key="index" class="column col text-center b">
+        <q-icon :name="stat.icon" size="xl" class="text-primary q-mx-auto" />
+        <q-item-section>
+          <q-item-label class="text-h4 text-bold q-pt-sm">{{ stat.value }}</q-item-label>
+          <q-item-label class="text-secondary">{{ stat.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+const stats = [
+  {
+    icon: 'hotel',
+    value: '154',
+    label: 'Yatak Sayısı',
+  },
+  {
+    icon: 'groups',
+    value: '27,500+',
+    label: 'Kapalı Alan',
+  },
+  {
+    icon: 'volunteer_activism',
+    value: '170',
+    label: 'Uzman Sağlık Personeli',
+  },
+  {
+    icon: 'verified',
+    value: '15',
+    label: 'Yıllık Tecrübe',
+  },
+]
+</script>
+
+<style scoped>
+.stats-banner {
+  width: 100%;
+  max-width: 1000px;
+  background-image: url('../assets/bg_stats.png');
+  background-size: cover;
+  background-position: center;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page padding>
     <ImageCarousel />
+    <StatisticsCard />
   </q-page>
 </template>
 
 <script setup>
 import ImageCarousel from 'src/components/ImageCarousel.vue'
+import StatisticsCard from 'src/components/StatisticsCard.vue'
 </script>
