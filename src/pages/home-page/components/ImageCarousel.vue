69ce2d19798ce9ea561a206a0d4acd33585ad3a8
Ali Valiyev <ali.valiyev.dev@gmail.com>
2025-04-30 00:41:33 +0400

    add links to slides

--- src/pages/home-page/components/ImageCarousel.vue
+++ src/pages/home-page/components/ImageCarousel.vue
@@ -5,7 +5,7 @@
       v-model="slide"
       padding
       infinite
-      autoplay
+      :autoplay="autoplay"
       swipeable
       arrows
       transition-prev="slide-right"
@@ -15,12 +15,36 @@
       control-color="secondary"
       class="carousel q-mx-auto"
     >
-      <q-carousel-slide :name="1" img-src="~assets/carousel/carousel_img_1.jpg" />
-      <q-carousel-slide :name="2" img-src="~assets/carousel/carousel_img_2.jpg" />
-      <q-carousel-slide :name="3" img-src="~assets/carousel/carousel_img_3.jpg" />
-      <q-carousel-slide :name="4" img-src="~assets/carousel/carousel_img_4.jpg" />
-      <q-carousel-slide :name="5" img-src="~assets/carousel/carousel_img_5.jpg" />
-      <q-carousel-slide :name="6" img-src="~assets/carousel/carousel_img_6.jpg" />
+      <q-carousel-slide
+        :name="1"
+        img-src="~assets/carousel/carousel_img_1.jpg"
+        @click="navigateTo('/check-up-kardiyoloji')"
+      />
+      <q-carousel-slide
+        :name="2"
+        img-src="~assets/carousel/carousel_img_2.jpg"
+        @click="navigateTo('/medicabil-aksam-poliklinik')"
+      />
+      <q-carousel-slide
+        :name="3"
+        img-src="~assets/carousel/carousel_img_3.jpg"
+        @click="navigateTo('/kaizen-award-turkiye-de-1-ve-dunyada-7-olduk')"
+      />
+      <q-carousel-slide
+        :name="4"
+        img-src="~assets/carousel/carousel_img_4.jpg"
+        @click="navigateTo('/ortopedide-robotik-cerrahi-tr')"
+      />
+      <q-carousel-slide
+        :name="5"
+        img-src="~assets/carousel/carousel_img_5.jpg"
+        @click="navigateTo('https://www.medicabilevde.com')"
+      />
+      <q-carousel-slide
+        :name="6"
+        img-src="~assets/carousel/carousel_img_6.jpg"
+        @click="navigateTo('https://online.fliphtml5.com/nqhcl/nztx/#p=1')"
+      />
     </q-carousel>
 
     <StatisticsCard class="gt-sm stats" />
@@ -29,9 +53,20 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import StatisticsCard from './StatisticsCard.vue'
 
 const slide = ref(1)
+const autoplay = ref(true)
+const router = useRouter()
+
+const navigateTo = (path) => {
+  if (path.startsWith('http://') || path.startsWith('https://')) {
+    window.open(path, '_blank')
+  } else {
+    router.push(path)
+  }
+}
 </script>
 
 <style scoped>
